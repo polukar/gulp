f598a5effe84996ab1b2b5926763ce7fd6221cd0
polukar <polukarov.qweewq@gmail.com>
2018-12-26 22:08:35 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,12 @@ const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config');
 const svgSprite = require('gulp-svg-sprite');
+const browserSync = require('browser-sync').create();
+const stream = browserSync.stream();
+const reload = browserSync.reload();
 
-function stylesLibs() {
-  return gulp.src('./src/sass/libs.scss')
+function stylesLibs(done) {
+  gulp.src('./src/sass/libs.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(minify())
     .pipe(autoprefixer({
@@ -19,28 +22,36 @@ function stylesLibs() {
       cascade: false
     }))
     .pipe(gulp.dest('./build/css'));
+  done();
 }
 
-function styles() {
-  return gulp.src('./src/sass/main.scss')
+function styles(done) {
+  gulp.src('./src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(stream);
+  done();
 }
 
-function script() {
-  return gulp.src('./src/js/main.js')
+function script(done) {
+  gulp.src('./src/js/main.js')
     .pipe(webpackStream(webpackConfig), webpack)
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./build/js'))
+    .pipe(stream);
+  done();
 }
 
-function html() {
-  return gulp.src('./src/pug/*.pug')
-    .pipe(pug({}))
+function html(done) {
+  gulp.src('./src/pug/*.pug')
+    .pipe(pug({
+      pretty: true,
+    }))
     .pipe(gulp.dest('./build'));
+  done();
 }
 
-function sprite() {
-  return gulp.src('./src/img/sprites/*.svg')
+function sprite(done) {
+  gulp.src('./src/img/sprites/*.svg')
     .pipe(svgSprite({
       mode: {
         css: {
@@ -51,16 +62,41 @@ function sprite() {
       }
     }))
     .pipe(gulp.dest('./build/img'));
+  done();
 }
 
-function img(){
-  
+function browserReload(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });
+  done();
 }
+
+// function img() {
+
+// }
 
 gulp.task('styleLibs', stylesLibs);
 gulp.task('styles', styles);
 gulp.task('script', script);
 gulp.task('html', html);
 gulp.task('sprite', sprite);
+gulp.task('browserReload', browserReload);
+
+function watch(done) {
+  gulp.watch('./src/sass/**/*.scss', gulp.parallel('styles'));
+  gulp.watch('./src/js/**/*.js', gulp.parallel('script'));
+  gulp.watch('./src/pug/**/*.pug', gulp.parallel('html'), reload);
+  done();
+}
+
+gulp.task('watch', watch, gulp.parallel(browserReload));
+
+
+gulp.task('default', gulp.parallel('styleLibs', 'styles', 'script', 'sprite', 'browserReload', 'html', 'watch'), false);
+
+
 
 
